f67fc64939c1004c8edf522ffc2eb2154588124d
Maciek W <maciej.wmroblewski@gmail.com>
2017-01-12 00:34:12 +0100

    kolejne zmiany. Ruszyy tylko jedno zdjecie w lewo

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 
 $(function(){
-	var carouselList = $("#carousel ul");
-	//var changeSlide = $("#carousel ul li");
-	carouselList.setInterval(carouselList, 3000);
-	carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
+	var carouselList = $("#carousel ul"),
+		changeSlide = $("#carousel ul li"),
+		firstItem = carouselList.find("li:first"),
+		lastItem = carouselList.find("li:last");
+
+	var moveFirstSlide = function(){
+		
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});;
+	};
+
+	var changeSlide = setInterval(function(){
+		firstItem.css("margin-left", "-400px");
+	}, 3000);
+
+	carouselList.animate({'marginLeft': -400}, 500, changeSlide, moveFirstSlide);
 });
 

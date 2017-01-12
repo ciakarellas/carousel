bb29721ece88dfc3f4d3e276527708d13f7fe114
Maciek W <maciej.wmroblewski@gmail.com>
2017-01-12 23:04:16 +0100

    W koncu dziala

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 
 $(function(){
-	var carouselList = $("#carousel ul"),
-		changeSlide = $("#carousel ul li"),
-		firstItem = carouselList.find("li:first"),
-		lastItem = carouselList.find("li:last");
-
+	var carouselList = $("#carousel ul")[0];
+			
+	debugger;
 	var moveFirstSlide = function(){
-		
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+
+
 		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});;
+		carouselList.css("margin-left", "0");
+
 	};
 
+
+
 	var changeSlide = setInterval(function(){
-		firstItem.css("margin-left", "-400px");
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
 	}, 3000);
 
-	carouselList.animate({'marginLeft': -400}, 500, changeSlide, moveFirstSlide);
+	
 });
 
